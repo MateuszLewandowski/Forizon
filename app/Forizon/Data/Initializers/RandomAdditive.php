4ef39d2b6510196221ba8c6c0294d78479c676f4
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-08-03 14:51:55 +0200

    Initializers update ranges & remove unnecessary

--- app/Forizon/Data/Initializers/RandomAdditive.php
+++ app/Forizon/Data/Initializers/RandomAdditive.php
@@ -7,15 +7,15 @@ use App\Forizon\Tensors\Matrix;
 
 class RandomAdditive implements Initializer
 {
-    private float $min;
-    private float $max;
+    private int $from;
+    private int $to;
 
-    public function __construct(float $min = 0.01, float $max = 0.1) {
-        $this->min = $min;
-        $this->max = $max;
+    public function __construct(int $from = -1, int $to = 1) {
+        $this->from = $from;
+        $this->to = $to;
     }
 
-    public function init(int $input, int $output): Matrix {
-        return Matrix::randomize($input, $output, $this->min, $this->max);
+    public function init(int $rows, int $columns): Matrix {
+        return Matrix::fillRandomize($rows, $columns, $this->from, $this->to);
     }
 }
